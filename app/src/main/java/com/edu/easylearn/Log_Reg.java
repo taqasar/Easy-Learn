ba92e2c36e266c110822f46eddb24e3a6ddcd90f
Taqasar Walayat <walayatt08@gmail.com>
2020-08-15 00:22:28 +0200

    Custom buttons and textViews solved - Activity linking solved

--- app/src/main/java/com/edu/easylearn/Log_Reg.java
+++ app/src/main/java/com/edu/easylearn/Log_Reg.java
@@ -2,9 +2,16 @@ package com.edu.easylearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Log_Reg extends AppCompatActivity {
+
+    private Button lg_btn;
+    private TextView reg_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,25 @@ public class Log_Reg extends AppCompatActivity {
         setContentView(R.layout.activity_log__reg);
 
         getSupportActionBar().hide();
+
+        lg_btn = findViewById(R.id.logReg_log_btn);
+
+        lg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent log_intent = new Intent(Log_Reg.this,Login.class);
+                startActivity(log_intent);
+            }
+        });
+
+        reg_txt = findViewById(R.id.Registrati);
+
+        reg_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg_intent = new Intent(Log_Reg.this,Register_User.class);
+                startActivity(reg_intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/edu/easylearn/Register_User.java
+++ app/src/main/java/com/edu/easylearn/Register_User.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class Register_User extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register__user);
 
         getSupportActionBar().hide();
     }
