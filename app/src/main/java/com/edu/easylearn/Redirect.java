9432bf02e18e6d2e1811e853dd9c2a97542eed89
Taqasar Walayat <walayatt08@gmail.com>
2020-08-29 20:27:18 +0200

    Added the progress bar for page loading and course redirection

--- app/src/main/java/com/edu/easylearn/Redirect.java
+++ app/src/main/java/com/edu/easylearn/Redirect.java
@@ -2,12 +2,16 @@ package com.edu.easylearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.ViewGroup;
 
 public class Redirect extends AppCompatActivity {
 
-    items itemsModel;
+    private Dialog loadingDialog;
+    Handler hndl = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,23 @@ public class Redirect extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        loadingDialog = new Dialog(Redirect.this);
+        loadingDialog.setContentView(R.layout.loading_progess_bar_page);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progress_background);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        loadingDialog.show();
+
         String s = getIntent().getStringExtra("DOVE");
 
         if (s != null && s.equals("kotlin")) {
-            startActivity(new Intent(Redirect.this, AndroidMenu.class));
+            hndl.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(Redirect.this, AndroidMenu.class));
+                    loadingDialog.cancel();
+                }
+            },1100);
         }
     }
 }
